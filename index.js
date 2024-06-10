ef39aef3b10d72cb93e337c9ddf187359a06f382
verbitskayaZoya <103507783+verbitskayaZoya@users.noreply.github.com>
2024-06-10 22:34:09 +0700

    add menu items section

--- index.js
+++ index.js
@@ -2,5 +2,23 @@ import { menuArray } from "./data.js"
 
 // just to check import/export => it works
 console.log(menuArray)
-// just to test js
-// document.querySelector("h1").style.color = "blue"
+
+// ----- getting elements from dom ---------------
+const menuItems = document.getElementById('menu-items')
+
+
+
+const generateHTMLtext = menuArray.map((item) => {
+    return ` <div class="menu-item-container"> 
+                <p class="menu-item-img"> ${item.emoji} </p>
+                <div class="menu-item-info">
+                    <h2> ${item.name} </h2>
+                    <p> ${item.ingredients} </p>
+                    <p> $${item.price} </p>
+                </div>
+                <img class="menu-item-add-btn" src="./assets/add-btn.png" />
+            </div>`
+}).join('')
+
+menuItems.innerHTML = generateHTMLtext
+
